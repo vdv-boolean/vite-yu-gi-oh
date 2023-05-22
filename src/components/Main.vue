56bc90c1a351f0d3aa6b1d49bf6d94aafef27709
vincenzodivita <divitavincenzo@icloud.com>
2023-05-22 14:31:23 +0200

    Add draft of cardlist with its style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,18 @@
 <script>
-
+    export default {};
 </script>
 
 <template>
- 
+    <div class="container">
+        Card list
+    </div>
 </template>
 
 <style scoped>
-
+    .container {
+        min-height: 500px;
+        width: 1000px;
+        margin: auto;
+        background-color: white;
+    }
 </style>

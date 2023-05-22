40f66b48521e0420575948384c5206e627310489
vincenzodivita <divitavincenzo@icloud.com>
2023-05-22 14:31:34 +0200

    Import components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup>
+<script>
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
 
+  export default {
+    components: {
+      Header,
+      Main,
+    },
+  };
 </script>
 
 <template>
  
+  <Header/>
+
+  <Main />
+
 </template>
 
 <style scoped>
